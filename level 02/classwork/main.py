20782e1321fd5db0f55fd6bed8d46b64a29bcdb6
ratibaghashvili <ratibaghashvili7@gmail.com>
2024-10-15 20:13:30 +0400

    third commit

---
+++ level 02/classwork/main.py
@@ -0,0 +1,15 @@
+# no classwork
+
+#1 classwork
+print("rati")
+print("baghashvili")
+print(14)
+
+#2 classwork
+name = "rati"
+surname = "baghashvili"
+age = 14
+
+print(name)
+print(surname)
+print(age)
